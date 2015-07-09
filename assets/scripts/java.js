ba6bce2450d46f80dfaeed581b26c3dca4660cce
joebegley15 <joebegley15@gmail.com>
2015-07-08 23:03:21 -0400

    It works. Just the basic tictactoeboard. With no score counting, but it works. LETS GO

--- assets/scripts/java.js
+++ assets/scripts/java.js
@@ -81,11 +81,16 @@ var changeTurn = function() {
   turn = (turn === "X" ? "O" : "X");
 };
 
-var getWinner = function() {
+var getWinnerX = function() {
 
   if (((board[0] === "X" && board[1] === "X" && board[2] === "X") || (board[3] === "X" && board[4] === "X" && board[5] === "X") || (board[6] === "X" && board[7] === "X" && board[8] === "X")) || ((board[0] === "X" && board[3] === "X" && board[6] === "X") || (board[1] === "X" && board[4] === "X" && board[7] === "X") || (board[2] === "X" && board[5] === "X" && board[8] === "X")) || ((board[0] === "X" && board[4] === "X" && board[8] === "X") || (board[2] === "X" && board[4] === "X" && board[6] === "X")))
-    alert("Congratulations Player X, you've won!")
+    alert("Congratulations Player X, you've won!");
   }
+
+var getWinnerO = function() {
+  if (((board[0] === "O" && board[1] === "O" && board[2] === "O") || (board[3] === "O" && board[4] === "O" && board[5] === "O") || (board[6] === "O" && board[7] === "O" && board[8] === "O")) || ((board[0] === "O" && board[3] === "O" && board[6] === "O") || (board[1] === "O" && board[4] === "O" && board[7] === "O") || (board[2] === "O" && board[5] === "O" && board[8] === "O")) || ((board[0] === "O" && board[4] === "O" && board[8] === "O") || (board[2] === "O" && board[4] === "O" && board[6] === "O")))
+    alert("Congratulations Player O, you've won!");
+}
 
 var clickHandler = function () {
   var $tile = $(this);
@@ -94,7 +99,8 @@ var clickHandler = function () {
   changeColor($tile);
   changeBoard($tile);
   changeTurn();
-  getWinner();
+  getWinnerX();
+  getWinnerO();
 };
 
 $(document).ready(function(){
