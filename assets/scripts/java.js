6764db3eec027d345727cc7f2782eface465c17a
joebegley15 <joebegley15@gmail.com>
2015-07-08 22:45:22 -0400

    Adding this before I readd the winner function and fuck everything up

--- assets/scripts/java.js
+++ assets/scripts/java.js
@@ -8,6 +8,57 @@ var changeBoard = function($dom) {
   if (turn === "X" && $dom.attr('id') === "tl") {
       board[0] = "X";
   }
+  if (turn === "X" && $dom.attr('id') === "tc") {
+      board[1] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "tr") {
+      board[2] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "cl") {
+      board[3] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "cc") {
+      board[4] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "cr") {
+      board[5] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "bl") {
+      board[6] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "bc") {
+      board[7] = "X";
+  }
+  if (turn === "X" && $dom.attr('id') === "br") {
+      board[8] = "X";
+  }
+  if (turn === "O" && $dom.attr('id') === "tl") {
+      board[0] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "tc") {
+      board[1] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "tr") {
+      board[2] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "cl") {
+      board[3] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "cc") {
+      board[4] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "cr") {
+      board[5] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "bl") {
+      board[6] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "bc") {
+      board[7] = "O";
+  }
+  if (turn === "O" && $dom.attr('id') === "br") {
+      board[8] = "O";
+  }
 };
 
 var changePiece = function($dom) {
@@ -37,12 +88,14 @@ var clickHandler = function () {
   changeColor($tile);
   changeBoard($tile);
   changeTurn();
+  getWinner();
 };
 
 $(document).ready(function(){
   var $tiles = $(".tiles");
 
   $tiles.on("click", clickHandler);
+
 //   $("#tc").on("click", function(){
 //     board[0] = "X"
 //   });
